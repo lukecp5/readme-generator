30b586bde16144cebc94765b6766bdf54d551c8d
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-18 10:01:45 -0400

    Add import statements and declare them as constants

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
             {
