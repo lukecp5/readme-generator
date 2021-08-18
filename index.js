b5efc7a8ec382e0a7f4e06b3b14a4215b1ff8ecf
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-18 09:54:22 -0400

    Add questions array, begin filling in questions with relevant variable names and messages

--- index.js
+++ index.js
@@ -2,7 +2,31 @@
 
 // TODO: Create an array of questions for user input
 const questions = [
-
+            {
+                  name: "username",
+                  type: "input",
+                  message: "What is your GitHub username?"
+            },{
+                  name: "project-name",
+                  type: "input",
+                  message: "What is the name of your project?"            
+            },{
+                  name: "email",
+                  type: "input",
+                  message: "What is your email address?" 
+            },{
+                  name: "username",
+                  type: "input",
+                  message: "What is your GitHub username?" 
+            },{
+                  name: "username",
+                  type: "input",
+                  message: "What is your GitHub username?" 
+            },{
+                  name: "username",
+                  type: "input",
+                  message: "What is your GitHub username?" 
+            }
 ];
 
 // TODO: Create a function to write README file
