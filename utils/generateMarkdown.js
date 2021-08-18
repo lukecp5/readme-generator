5f7c2117ecf0ecf9ba67b0b44a2e660537b6b446
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-18 10:11:26 -0400

    Add license handler to generateMarkdown. Takes license selection and uses github badge for that license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if(license != "None"){
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
