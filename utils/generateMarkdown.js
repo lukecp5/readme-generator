390eadd29584090e4da4ceb1a207cbb53fcf2d66
Luke Poirrier <lukepoirrier5@gmail.com>
2021-08-18 21:35:44 -0400

    Finish creating an object to hold the URL of each option in the inquirer prompt for the licenses section. Still need to figure out how to select the correct license urls for the  licenses that have spaces in the name.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,15 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 const licenseURLS = {
   MIT: "https://opensource.org/licenses/MIT",
   ISC: "https://opensource.org/licenses/ISC",
   "APACHE 2.0": "https://opensource.org/licenses/Apache-2.0",
+  "BSD 3": "https://opensource.org/licenses/BSD-3-Clause",
+  "GPL 3.0": "https://www.gnu.org/licenses/gpl-3.0",
+  "IPL 1.0": "https://opensource.org/licenses/IPL-1.0",
+  "MPL 2.0": "https://opensource.org/licenses/MPL-2.0",
+  "Unlicense": "http://unlicense.org/"
 };
+// TODO: Create a function that returns a license badge based on which license is passed in
+// ! If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license != "None") {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
@@ -14,7 +19,12 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license != "None"){
+
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
